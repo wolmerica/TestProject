88dbcafc0eab24106ccf9df61be34ecbabf3a619
Richard Wolschlager <wolmerica@gmail.com>
2019-01-21 16:03:49 -0500

    Adding Focus class

---
+++ coding/src/main/java/wolmerica/coding/Focus.java
@@ -0,0 +1,30 @@
+package wolmerica.coding;
+
+public class Focus implements IAutomobile {
+
+	public void retailPrice() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void wheelDrive() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void passengerCapacity() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void propulsionType() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void efficiencyRate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
